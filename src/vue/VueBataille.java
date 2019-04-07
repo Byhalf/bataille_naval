87e0c36c74632032e5e5ecf09e7627e046874906
laurine <21705826@etu.unicaen.fr>
2019-04-07 18:51:41 +0200

    Correction de VueBataille

--- src/vue/VueBataille.java
+++ src/vue/VueBataille.java
@@ -8,15 +8,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class VueBataille extends JPanel implements EcouteurModele {
-    public static final int TAILLE_CASE = 10;
+    public static final int TAILLE_CASE = 50;
+    public static final int TAILLE_GRILLE = 10;
     private int dimX, dimY;
     private Modele modele;
 
     public VueBataille(Modele modele) {
         //modele.ajoutEcouteur(this);
         this.modele = modele;
-        dimX = 300;
-        dimY = 300;
+        dimX = 500;
+        dimY = 500;
         setPreferredSize(new Dimension(dimX, dimY));
     }
 
@@ -31,15 +32,9 @@ public class VueBataille extends JPanel implements EcouteurModele {
 
         Bateau[][] grille1 = modele.getJoueur1().getGrille();
 
-        for (int j = 0;j<dimX;j++){
-            for (int i = 0; i < dimY; i++) {
-                if (grille1[i][j] == null) {
-                    if (modele.getMer().getCaseTireJ1()[i][j]) {
-                        g.drawRect(i*TAILLE_CASE,j*TAILLE_CASE,10,10);
-                    } else {
-                        g.fillRect(i*TAILLE_CASE,j*TAILLE_CASE,10,10);
-                    }
-                }
+        for (int i = 0; i < TAILLE_GRILLE; i++) {
+            for (int j = 0; j < TAILLE_GRILLE ; j++) {
+                g.drawOval(i*TAILLE_CASE,j*TAILLE_CASE,TAILLE_CASE,TAILLE_CASE);
             }
         }
     }
