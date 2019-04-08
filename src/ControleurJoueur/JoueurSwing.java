6ba530549f4d608ae7eb63a933ef16245eb1d0a2
Byhalf <gareth.lafay@gmail.com>
2019-04-08 07:24:29 +0200

    modification joueurSwing c'est pas fini

--- src/ControleurJoueur/JoueurSwing.java
+++ src/ControleurJoueur/JoueurSwing.java
@@ -8,14 +8,17 @@ import modele.joueurs.Joueur;
 import modele.utilities.Coordonnees;
 import vue.GameVue;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class JoueurSwing extends Controleur {
+public class JoueurSwing extends Controleur implements MouseListener {
     private GameVue gameVue;
     private Modele modele;
     int TAILLE_GRILLE = 10;
     private Random randomGenerator = new Random();
+    private Coordonnees mousePos:
 
     public JoueurSwing(Joueur joueurControle, GameVue gameVue, Modele modele) {
         super(joueurControle);
@@ -23,9 +26,31 @@ public class JoueurSwing extends Controleur {
         this.modele = modele;
     }
 
+    public void attend() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public Coordonnees mousePosConvertisseur(int x, int y) {
+        int resX, resY;
+        for (int i = 0; i < TAILLE_GRILLE; i++) {
+            for (int j = 0; j < TAILLE_GRILLE; j++) {
+
+            }
+        }
+    }
     @Override
     public Coordonnees choixTir() {
-        return new Coordonnees(0, 0);
+        Coordonnees oldCoord = new Coordonnees(mousePos.getX(), mousePos.getY());
+        //on ajoute l'écouteur à la grille concerné
+        gameVue.getVueGrille2().addMouseListener(this);
+        while (oldCoord.compare(mousePos)) {
+            attend();
+        }
+        return mousePos;
     }
 
     @Override
@@ -44,5 +69,9 @@ public class JoueurSwing extends Controleur {
         return possibleRes.get(randomGenerator.nextInt(possibleRes.size()));
     }
 
-
+    //modifie mousePos
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        mousePos = new Coordonnees(e.getX())
+    }
 }
