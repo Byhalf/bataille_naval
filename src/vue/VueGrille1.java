2a37d6666d335cf8935326a9853b31b6f2c91590
laurine <21705826@etu.unicaen.fr>
2019-04-08 16:05:59 +0200

    b

--- src/vue/VueGrille1.java
+++ src/vue/VueGrille1.java
@@ -44,7 +44,7 @@ public class VueGrille1 extends JPanel implements VueDessinable {
                             g.fillOval(i * TAILLE_CASE, j * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
                         }
                     } else {
-                        g.setColor(Color.blue);
+                        g.setColor(Color.gray);
                         g.fillOval(i * TAILLE_CASE, j * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
                     }
                 } else {
