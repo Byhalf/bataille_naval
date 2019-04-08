2ea5db0f7dc090ab75615250f970d6bb799076fc
Byhalf <gareth.lafay@gmail.com>
2019-04-08 22:54:31 +0200

    modification testfin

--- src/tests/TestFin.java
+++ src/tests/TestFin.java
@@ -13,13 +13,15 @@ public class TestFin {
     public TestFin() throws Exception {
     }
 
-
+    /**
+     * La partie n'est pas fini si un des bateaux n'est pas coulé, ici il n'y a pas de bateau
+     * la partie doit donc etre fini.
+     */
     @Test
     public void testFin(){
         joueur1 = new Joueur("premier");
         joueur2 = new Joueur("deuxième");
         mer = new Mer(joueur1, joueur2);
-        mer.aPerdu(joueur1);
         fin=mer.estFini();
 
         Assertions.assertTrue(fin==true);
